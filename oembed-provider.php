9945b0669ddccb535d6c2b456ae24a395a3c11bd
Matthias Pfefferle <pfefferle@gmail.com>
2011-09-30 11:15:26 +0200

    initial release

---
+++ oembed-provider.php
@@ -0,0 +1,110 @@
+<?php
+/*
+Plugin Name: oEmbedProvider
+Plugin URI: https://github.com/pfefferle/oEmbedProvider
+Description: An oEmbed provider for Wordpress
+Version: 1.0
+Author: Matthias Pfefferle
+Author URI: http://notizblog.org/
+*/
+
+/*
+Based on the plugin of Craig Andrews <candrews@integralblue.com> http://candrews.integralblue.com
+*/
+
+add_action('wp_head', array('OembedProvider', 'add_oembed_links'));
+add_action('parse_query', array('OembedProvider', 'print_oembed_file'));
+add_filter('query_vars', array('OembedProvider', 'queryvars'));
+
+class OembedProvider {
+	function add_oembed_links(){
+    if(is_single() || is_page() || is_attachment()){
+      print '<link rel="alternate" type="application/json+oembed" href="' . get_bloginfo('wpurl') . '/?format=json&amp;oembed=' . urlencode(get_permalink())  . '" />';
+      print '<link rel="alternate" type="application/xml+oembed" href="' . get_bloginfo('wpurl') . '/?format=xml&amp;oembed=' . urlencode(get_permalink())  . '" />';
+    }
+  }
+	
+	function queryvars($queryvars) {
+		$queryvars[] = 'oembed';
+		$queryvars[] = 'format';
+	  return $queryvars;
+	}
+	
+	function print_oembed_file($wp_query) {
+		if (!isset($wp_query->query_vars['oembed'])) {
+	    return;
+	  }
+
+		$post_ID = url_to_postid($wp_query->query_vars['oembed']);
+	  $post = get_post($post_ID);
+	  if(empty($post)) {
+	    header('Status: 404');
+	    die("Not found");
+	  } else {
+	    $author = get_userdata($post->post_author);
+	    $oembed=array();
+	    $oembed['version']='1.0';
+	    $oembed['provider_name']=get_option('blogname');
+	    $oembed['provider_url']=get_option('home');
+	    $oembed['author_name']=$author->display_name;
+	    $oembed['author_url']=get_author_posts_url($author->ID, $author->nicename);
+	    $oembed['title']=$post->post_title;
+
+	    switch(get_post_type($post)) {
+	      case 'attachment':
+	        if(substr($post->post_mime_type,0,strlen('image/'))=='image/'){
+	          $oembed['type']='photo';
+	        } else {
+	          $oembed['type']='link';
+	        }
+	        $oembed['url']=wp_get_attachment_url($post->ID);
+	        break;
+	      case 'post':
+	      case 'page':
+	        if (function_exists('has_post_thumbnail') && has_post_thumbnail($post->ID)) {
+						$image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID));
+						$oembed['thumbnail_url'] = $image[0];
+						$oembed['thumbnail_width'] = $image[1];
+						$oembed['thumbnail_height'] = $image[2];
+					}
+	        $oembed['type']='rich';
+	        $oembed['html']=empty($post->post_excerpt)?$post->post_content:$post->post_excerpt;
+	        break;
+	      default:
+	        header('Status: 501');
+	        die('oEmbed not supported for posts of type \'' . $post->type . '\'');
+	        break;
+	      }
+
+	      $format = $_GET['format'];
+	      switch($format){
+	        case 'xml':
+	          header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
+            print '<?xml version="1.0" encoding="' . get_option('blog_charset') . '" standalone="yes"?>';
+	          print '<oembed>';
+	          foreach(array_keys($oembed) as $element){
+	            print '<' . $element . '>' . htmlentities($oembed[$element]) . '</' . $element . '>';
+	          }
+	          print '</oembed>';
+	          break;
+	        case 'json':
+				  default:
+		        header('Content-Type: application/json; charset=' . get_option('blog_charset'), true);
+		        $callback = $_GET['callback'];
+		        if($callback){
+		          print $callback . '(';
+		        }
+		        print(json_encode($oembed));
+		        if($callback){
+		          print ');';
+		        }
+		        break;
+	            
+	          header('Status: 501');
+	          die('Format \'' . $format . '\' not supported');
+	        }
+	    }
+		exit;
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== oEmbed Provider ===
+Contributors: pfefferle
+Donate link: http://14101978.de/
+Tags: oembed, links
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: 1.0
+
+An oEmbed provider for Wordpress. Based on the work of [Craig Andrews](http://candrews.integralblue.com "Craig Andrews")!
+
+== Description ==
+
+The oEmbed provider plugin makes Wordpress an oEmbed provider, compliant with the XML and JSON specification at http://www.oembed.com.
+
+oEmbed is a powerful protocol that allows sites to automatically embed content from 3rd parties directly into their site in whatever way they choose. For example, if a user on http://identi.ca links to a Wordpress blog with this plugin enabled, the link, when clicked, will show an excerpt from the blog post, the authors name, and various links... automatically.
+
+== Installation ==
+
+1. Upload the plugin to `oembed-provider.php` in the `/wp-content/plugins/oembed-provider/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Step 3? There is no step 3!
+
+== Frequently Asked Questions ==
+
+= Is it really that easy to install this plugin? =
+
+Yes!
+
+== Changelog ==
+
+= 1.1 =
+* Added callback parameter for JSON-P
+
+= 1.0 =
+* Initial release
